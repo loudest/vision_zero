56cfe94fb67bea50755df0dd4dea0951aea727c2
Alexander <alexandermichaelmiranda@gmail.com>
2015-03-21 14:05:38 -0700

    Updated the api

--- api/app.py
+++ api/app.py
@@ -1,11 +1,18 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "Hello, World!"
+
+@app.route('/signed_data', methods=['GET'])
+def signed_map():
+  r = requests.get('http://data.seattle.gov/resource/kb3s-zi3s.json')
+  json_data = r.json()
+  return jsonify({'data': json_data})
 
 if __name__ == '__main__':
   app.run(debug=True)
