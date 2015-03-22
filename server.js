9de9b7daad53f6907728a236a603be55011c51aa
GHowlett <griff.howl@gmail.com>
2015-03-22 07:53:12 -0700

    collision data served from AWS RDS PostGis Instance

--- server.js
+++ server.js
@@ -1,157 +1,47 @@
 var path = require('path');
 var http = require('http');
 var express = require('express');
+var pg = require('pg');
 
 var server = express()
 	.use(express.static(__dirname + '/www'))
 	.use(express.static(__dirname + '/bower_components'))
 	.use(express.bodyParser())
 
-var fakeData = {
-	"type": "FeatureCollection",
-	"crs": {
-	    "type": "name",
-	    "properties": {
-	        "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-	    }
-	},
-	"features": [{
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Overcast",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.3687473899999, 47.67381616900008]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Dark - Street Lights On"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.32348092899991, 47.637531713000044]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.39287315199994, 47.668660524000074]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Overcast",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.36099416999991, 47.68179741800009]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Raining",
-	        "lighting_condition": "Dark - Street Lights On"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.3448083539999, 47.614710262000074]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Raining",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.33716242999992, 47.60235325700006]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Dark - Street Lights On"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.31563384899994, 47.619951977000085]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.33748095199991, 47.61682684500005]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Raining",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.3400282589999, 47.60882663700005]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Overcast",
-	        "lighting_condition": "Dark - Street Lights On"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.33285960599994, 47.60266701300009]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.38760217399994, 47.67019286900006]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Unknown",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.33017730199992, 47.661374812000076]
-	    }
-	}, {
-	    "type": "Feature",
-	    "properties": {
-	        "weather_condition": "Clear or Partly Cloudy",
-	        "lighting_condition": "Daylight"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [-122.33957314499992, 47.608329076000075]
-	    }
-	}]
-};
-
 server.get('/crashdata.json', function(req,res){
 
-	res.json(fakeData);
-	
+	// TODO: get loc from querystring
+	var fakeLoc = [-122.32348092899991,47.637531713000044] // long,lat
+	var conString = "postgres://awsuser:Password@vision-zero.czkztglgrevo.us-west-2.rds.amazonaws.com/vision_zero";
+
+	pg.connect(conString, function(err, client){
+	  if (err) return console.error('error fetching client from pool', err);
+	  
+	  // all data within 0.5 miles
+	  var query = 'select weather_condition, lighting_condition, ST_AsGeoJSON(wkb_geometry) as geometry from collision_data where ST_Distance_Sphere(wkb_geometry, ST_MakePoint($1,$2)) <= 0.5 * 1609.34';
+	  client.query(query, fakeLoc, function(err, result) {
+	    if (err) return console.error('error running query', err);
+	   	
+	    var geoJson = {
+		    type: "FeatureCollection",
+		    features: []
+		}
+
+		result.rows.forEach(function(row){
+			geoJson.features.push({
+        		"type": "Feature",
+        		"properties": {
+           			"weather_condition": row.weather_condition,
+            		"lighting_condition": row.lighting_condition,
+        		},
+        		"geometry": JSON.parse(row.geometry)
+			});
+		});
+
+	    res.json(geoJson);
+	    client.end();
+	  });
+	});
 });
 
 var port = process.env.PORT || 3000;
--- www/script.js
+++ www/script.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  var map = L.map('map').setView([47.6097, -122.3331], 13);
+  var map = L.map('map').setView([47.6097, -122.3331], 16);
 
   L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -9,7 +9,6 @@ $(function(){
   }).addTo(map);
 
   $.getJSON('crashdata.json', function(data){
-  	console.log(data);
   	L.geoJson(data).addTo(map);
   });
 
