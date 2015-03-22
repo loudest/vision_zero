592d87dda08a41bb753118198bee1b2e118de41e
GHowlett <griff.howl@gmail.com>
2015-03-22 02:12:54 -0700

    switched to mapquest tiling

--- www/script.js
+++ www/script.js
@@ -1,8 +1,11 @@
 $(function(){
 
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([47.6097, -122.3331], 13);
 
-  var googleLayer = new L.Google('ROADMAP');
-  map.addLayer(googleLayer);
+  L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    subdomains: '1234',
+  }).addTo(map);
 
 });
